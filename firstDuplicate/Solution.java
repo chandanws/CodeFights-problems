33c742489cae6426a5f22a67d7e19d910c30f507
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-05-15 18:51:02 -0700

    Create Solution.java

---
+++ firstDuplicate/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+  int firstDuplicate(int[] a) {
+    int len = a.length;
+    for(int i = 0; i < len; i++) {
+        int index = Math.abs(a[i]);
+        if(a[index - 1] < 0) return index;
+        a[index - 1] *= -1;
+    }
+    return -1;
+  }
+}
